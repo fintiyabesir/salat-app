97870f68eaf421b0733ccbd1a1d3e89d598faa4c
fintiyabesir <besir@fintiya.com>
2026-08-26 20:08:37 +0300

    Add offline iOS Hijri date formatter

---
+++ iosApp/SalatApp/IOSHijriFormatter.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct IOSHijriFormatter {
+    static func format(
+        date: Date,
+        timeZone: TimeZone,
+        method: String,
+        dayAdjustment: Int
+    ) -> String {
+        var gregorian = Calendar(identifier: .gregorian)
+        gregorian.timeZone = timeZone
+        let adjusted = gregorian.date(
+            byAdding: .day,
+            value: min(2, max(-2, dayAdjustment)),
+            to: date
+        ) ?? date
+
+        let identifier: Calendar.Identifier
+        switch method {
+        case "TABULAR":
+            identifier = .islamicTabular
+        case "UMM_AL_QURA", "AUTOMATIC":
+            identifier = .islamicUmmAlQura
+        default:
+            identifier = .islamicUmmAlQura
+        }
+
+        var hijri = Calendar(identifier: identifier)
+        hijri.timeZone = timeZone
+        hijri.locale = L10n.selectedLocale
+
+        let formatter = DateFormatter()
+        formatter.calendar = hijri
+        formatter.timeZone = timeZone
+        formatter.locale = L10n.selectedLocale
+        formatter.dateFormat = "d MMMM y"
+        return formatter.string(from: adjusted)
+    }
+}
