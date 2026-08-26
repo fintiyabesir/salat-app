32fb662c76769843b5b077b06a58e2cc27bd919b
fintiyabesir <besir@fintiya.com>
2026-08-26 03:59:40 +0300

    Add regional resolver tests

---
+++ shared/src/commonTest/kotlin/app/salat/domain/RegionalCalculationProfileResolverTest.kt
@@ -0,0 +1,18 @@
+package app.salat.domain
+
+import app.salat.model.CalculationMethodId
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RegionalCalculationProfileResolverTest {
+    @Test fun known_regions_select_expected_fallbacks() {
+        assertEquals(CalculationMethodId.TURKEY, RegionalCalculationProfileResolver.resolve("TR").method)
+        assertEquals(CalculationMethodId.SINGAPORE, RegionalCalculationProfileResolver.resolve("SG").method)
+        assertEquals(CalculationMethodId.EGYPTIAN, RegionalCalculationProfileResolver.resolve("EG").method)
+        assertEquals(CalculationMethodId.UMM_AL_QURA, RegionalCalculationProfileResolver.resolve("SA").method)
+    }
+
+    @Test fun unknown_region_uses_mwl_as_safe_generic_fallback() {
+        assertEquals(CalculationMethodId.MUSLIM_WORLD_LEAGUE, RegionalCalculationProfileResolver.resolve("ZZ").method)
+    }
+}
