80ffa936652d4b86ba169a689490f47bf9046b6c
fintiyabesir <besir@fintiya.com>
2026-08-26 17:17:22 +0300

    Track exact-alarm permission changes

--- androidApp/src/main/java/app/salat/mobile/PrayerScheduleLifecycleReceiver.kt
+++ androidApp/src/main/java/app/salat/mobile/PrayerScheduleLifecycleReceiver.kt
@@ -1,5 +1,6 @@
 package app.salat.mobile
 
+import android.app.AlarmManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -20,7 +21,8 @@ class PrayerScheduleLifecycleReceiver : BroadcastReceiver() {
             Intent.ACTION_BOOT_COMPLETED,
             Intent.ACTION_TIMEZONE_CHANGED,
             Intent.ACTION_TIME_CHANGED,
-            Intent.ACTION_MY_PACKAGE_REPLACED
+            Intent.ACTION_MY_PACKAGE_REPLACED,
+            AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED
         )
     }
 }
