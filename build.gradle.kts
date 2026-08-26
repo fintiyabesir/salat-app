4b4eab10a576935f1f9523e1f155e356380dee07
fintiyabesir <besir@fintiya.com>
2026-08-26 04:05:16 +0300

    Fix version catalog plugin accessors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.androidKmpLibrary) apply false
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.kmp.library) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
